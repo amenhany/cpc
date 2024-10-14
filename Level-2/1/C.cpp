e9b37a6cfed86c9dd8b31c978ac7dd8d05685638
Amen Hany <amen.hany.2810@gmail.com>
2024-10-14 12:49:41 +0300

    lvl-2/1/C Tavas and SaDDas

---
+++ Level-2/1/C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+using std::sort;
+typedef long long ll;
+
+
+vector<long long> luckyNums;
+
+void solve(long long n) {
+    if (n > 1e9) return;
+    if (n > 0) luckyNums.push_back(n);
+
+    solve(n*10 + 4);
+    solve(n*10 + 7);
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    long long n;
+    cin >> n;
+    solve(0);
+
+    sort(luckyNums.begin(), luckyNums.end());
+    for (long long i = 0; i < luckyNums.size(); i++)
+        if (luckyNums[i] == n) cout << i+1 << '\n';
+}
