d3445a0b2866ff9fdd152bc86581c5cf21ccb39b
Amen Hany <amen.hany.2810@gmail.com>
2024-10-16 23:06:33 +0300

    1/A 822A I'm bored with life

--- Level-2/1/A.cpp
+++ Level-2/1/A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
 using std::cin;
 using std::cout;
@@ -9,9 +10,20 @@ using std::vector;
 typedef long long ll;
 
 
+ll factorial(ll n) {
+    ll result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    
+    ll a, b;
+    cin >> a >>b;
+
+    cout << factorial(std::min(a, b));
 }
